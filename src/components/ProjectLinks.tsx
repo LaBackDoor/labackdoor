bd56c84e2496899cafed1f0fa247abe32b91610c
webster elumelu <elumeluwebster@outlook.com>
2024-11-28 17:23:24 +0100

    changed ProjectLinks text animation edited out unused portion of stylessheet

--- src/components/ProjectLinks.tsx
+++ src/components/ProjectLinks.tsx
@@ -9,7 +9,7 @@ const ProjectLinks = () => {
 
     const projects = [
         {
-            name: 'AW-TRBAC',
+            name: 'AW-\nTRBAC',
             year: '2024',
             link: "https://www.nlpaics.com/accepted-papers"
         },
@@ -19,20 +19,19 @@ const ProjectLinks = () => {
             link: "https://itrust.sutd.edu.sg/first-international-conference-on-the-design-of-cyber-secure-water-plants-dcs-water24/programme-dcs-water24/"
         },
         {
-            name: 'Crowdsourcing',
+            name: 'Crowd\nsourcing',
             year: '2024',
             link: "https://asonam.cpsc.ucalgary.ca/2024/AcceptedPapers.php"
         },
     ];
 
     return (
-        <div className="flex items-center justify-end w-full h-full pr-12 mt-28">
-            <ul className="flex flex-col items-end gap-12 [perspective:1000px]">
+        <div className="flex items-center justify-end w-full h-full mt-28">
+            <ul className="flex flex-col items-end gap-12 overflow-hidden">
                 {projects.map((project, index) => (
                     <li
                         key={project.name}
-                        className={`relative cursor-pointer transition-all duration-500 ease-out transform-gpu
-                    ${hoveredIndex === index ? '[transform:translateZ(50px)_rotateY(15deg)]' : 'transform-none'}`}
+                        className="relative cursor-pointer w-[250%] text-right overflow-hidden"
                         onMouseEnter={() => setHoveredIndex(index)}
                         onMouseLeave={() => setHoveredIndex(null)}
                     >
@@ -40,14 +39,13 @@ const ProjectLinks = () => {
                             href={project.link}
                             target="_blank"
                             rel="noopener noreferrer"
-                            className={`relative block text-[7vw] leading-[0.9] font-black uppercase tracking-wider transform origin-right whitespace-pre-line transition-all duration-500
-                                `}
-                            /*${hoveredIndex === index ? 'hover:text-outline hover:text-transparent' : ''}*/
+                            className={`relative inline-block text-[7vw] leading-[0.9] font-extrabold uppercase tracking-wider whitespace-pre-line transition-transform duration-[1500ms] ease-out font-druk
+                                ${hoveredIndex === index ? 'translate-x-[50%]' : 'translate-x-0'}`}
                             data-info={project.year}
                         >
                             <span className="relative inline-block">
                                 {project.name}
-                                <span className="absolute -left-[200px] top-[3.7em] w-[150px] text-right text-[0.7vw] font-medium opacity-75">
+                                <span className="absolute -left-[200px] top-[3.7em] w-[150px] text-right text-[0.7vw] font-medium opacity-75 transition-none transform-none pointer-events-none">
                                     {project.year}
                                 </span>
                                 <span className="absolute top-[0.23em] -left-[0.3em] w-[2px] h-[0.85em] bg-current rotate-[25deg]" />
