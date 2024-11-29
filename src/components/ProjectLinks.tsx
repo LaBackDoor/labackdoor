cecd4b4703a1c07a756be07e399aa5c473730c4f
webster elumelu <elumeluwebster@outlook.com>
2024-11-29 18:13:05 +0100

    slash fixes
    added inheritance to the colors of the span near the year

--- src/components/ProjectLinks.tsx
+++ src/components/ProjectLinks.tsx
@@ -10,20 +10,17 @@ const ProjectLinks = () => {
     const projects = [
         {
             name: 'AW-TRBAC',
-            slash: '/',
-            year: '2024 ',
+            year: '2024',
             link: "https://www.nlpaics.com/accepted-papers"
         },
         {
             name: 'MRNNs',
-            slash: '/',
-            year: '2024 ',
+            year: '2024',
             link: "https://itrust.sutd.edu.sg/first-international-conference-on-the-design-of-cyber-secure-water-plants-dcs-water24/programme-dcs-water24/"
         },
         {
             name: 'Crowdsourcing',
-            slash: '/',
-            year: '2024 ',
+            year: '2024',
             link: "https://asonam.cpsc.ucalgary.ca/2024/AcceptedPapers.php"
         },
     ];
@@ -42,7 +39,7 @@ const ProjectLinks = () => {
                             href={project.link}
                             target="_blank"
                             rel="noopener noreferrer"
-                            className={`relative project-link inline-block text-[7vw] leading-[0.9] font-black uppercase tracking-tighter transition-transform duration-[1500ms] ease-out font-druk
+                            className={`relative group project-link inline-block text-[7vw] leading-[0.9] font-black uppercase tracking-tighter transition-transform duration-[1500ms] ease-out font-druk
                                 ${hoveredIndex === index ? 'translate-x-[30%]' : 'translate-x-0'}`}
                             data-info={project.year}
                         >
@@ -51,9 +48,9 @@ const ProjectLinks = () => {
                                     {project.name}
                                 </span>
                                 <span className="absolute project-year -left-[200px] top-[3.7em] w-[150px] text-right text-[0.7vw] font-medium opacity-75 transition-none transform-none pointer-events-none">
-                                    {project.year}
+                                    {`${project.year} \t`}
                                 </span>
-                                <span className="absolute top-[0em] transition-none will-change-transform border border-dotted left-[-0.3em] w-[2px] h-[0.85em] bg-current rotate-[25deg]" />
+                                <span className="absolute top-[0em] transition-none will-change-transform border border-solid border-current left-[-0.3em] w-[2px] h-[0.85em] bg-current rotate-[25deg] group-hover:border-current group-hover:bg-current group-hover:border-solid" />
                             </span>
                         </a>
                     </li>
