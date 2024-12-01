63e9f765d40a66c9a6a3145128a010b596b9a5e2
webster elumelu <elumeluwebster@outlook.com>
2024-12-01 13:09:50 +0100

    added: routing for pages styling for Akzidenz font contact page with optional layout icons

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+declare module '*.svg' {
+    const content: string;
+    export default content;
+}
+
+
+// in case we need to use svg as a react component as a change of heart/mind
+
+{/*
+    declare module '*.svg' {
+    import type * as React from 'react';
+    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    export default src;
+}
+*/}
